86085798707f1620e76ea7a1687a02167d2a804b
skoot1331 <114890628+skoot1331@users.noreply.github.com>
2023-05-08 15:15:38 -0400

    added constants and vendor libraries

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,6 +14,36 @@ package frc.robot;
  */
 public final class Constants {
   public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
+    public static final int kDriverControllerPort = 2;
+    public static final int kOperatorControllerPort = 0;
   }
+  public static final double DRIVETRAIN_TRACKWIDTH_METERS = 0.559;
+  public static final double DRIVETRAIN_WHEELBASE_METERS = 0.559;
+
+  public static final int DRIVETRAIN_PIGEON_ID = 0;
+
+  public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 7;
+  public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 8;
+  public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 13;
+  public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(280.0);
+
+  public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 1;
+  public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 2;
+  public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 10;
+  public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(309.0);
+
+  public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 5;
+  public static final int BACK_LEFT_MODULE_STEER_MOTOR = 6;
+  public static final int BACK_LEFT_MODULE_STEER_ENCODER = 12;
+  public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(332.0);
+
+  public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 3;
+  public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 4;
+  public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 11;
+  public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(276.0);
+
+  // These variables allow the speed and rotation of the robot to be reduced independently
+  // This really should be handled with a PID
+  public static final double MAX_SPEED = 0.25;
+  public static final double MAX_ROTATION = 0.25;
 }
