174b39ca0d669d04a057751c0a4282d8e655f2c9
mel813 <melissafroh@gmail.com>
2023-05-08 15:33:26 -0400

    Added constant variables to define speed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,7 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+
+  //public static final int kAutoDriveDistanceMeters = 10;
+  public static final int kAutotDriveSpeed = 15;
 }
